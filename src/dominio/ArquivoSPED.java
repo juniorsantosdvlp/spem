a7c4e521f9235174ca9b316e4ef0ea4a0599a0ca
Juniordvlp <ermiliosantos@unigranrio.br>
2016-11-10 14:37:00 -0200

    commit segundo

--- src/dominio/ArquivoSPED.java
+++ src/dominio/ArquivoSPED.java
@@ -40,33 +40,83 @@ public class ArquivoSPED {
 		int indice = 0;
 		int c100index = 0;
 		Double baseIcms = 0.0;
-		Double totalIcms = 0.0;
+		Double baseIcms27 = 0.0;
+		Double totalIcms20 = 0.0;
 		Double icmsAntigo = 0.0;
 		Double totalIcms19 = 0.0;
+		Double totalIcms26 = 0.0;
+		Double totalIcms27 = 0.0;
 
 		for (String[] strings : arquivoArray) {
 			if (strings[0].equals("C100")) {
 				c100index = indice;
-			} else if (strings[0].equals("C170") && strings[13].equals("19,00")) {
+			} else if (strings[0].equals("C170") && strings[13].length() > 1
+					&& strings[13].substring(0, 2).equals("19")) {
 				baseIcms = Double.parseDouble(strings[12].replaceAll(",", "."));
-				totalIcms = baseIcms * 0.2;
+				totalIcms20 = baseIcms * 0.2;
 
-				strings[13] = "20,00";
-				strings[14] = String.format("%.2f", totalIcms);
+				strings[13] = "20";
+				strings[14] = String.format("%.2f", totalIcms20);
 
-			} else if (strings[0].equals("C190") && strings[3].equals("19,00")) {
+			} else if (strings[0].equals("C170") && strings[13].length() > 1
+					&& strings[13].substring(0, 2).equals("26")) {
+				baseIcms27 = Double.parseDouble(strings[12].replaceAll(",", "."));
+				totalIcms27 = baseIcms27 * 0.27;
+
+				strings[13] = "27";
+				strings[14] = String.format("%.2f", totalIcms27);
+
+			} else if (strings[0].equals("C190") && strings[3].length() > 1
+					&& strings[3].substring(0, 2).equals("19")) {
 				strings[3] = "20,00";
 				baseIcms = Double.parseDouble(strings[5].replaceAll(",", "."));
-				totalIcms = baseIcms * 0.2;
+				totalIcms20 = baseIcms * 0.2;
 				totalIcms19 = Double.parseDouble(strings[6].replaceAll(",", "."));
-				strings[6] = String.format("%.2f", totalIcms);
-				totalIcms =  totalIcms - totalIcms19;
-				icmsAntigo = Double.parseDouble(arquivoArray.get(c100index)[21].replaceAll(",", ".")) + totalIcms;
-				
-				
-				
+				strings[6] = String.format("%.2f", totalIcms20);
+				totalIcms20 = totalIcms20 - totalIcms19;
+				icmsAntigo = Double.parseDouble(arquivoArray.get(c100index)[21].replaceAll(",", ".")) + totalIcms20;
+
 				arquivoArray.get(c100index)[21] = String.format("%.2f", icmsAntigo);
 
+			} else if (strings[0].equals("C190") && strings[3].length() > 1
+					&& strings[3].substring(0, 2).equals("26")) {
+				strings[3] = "27,00";
+				baseIcms27 = Double.parseDouble(strings[5].replaceAll(",", "."));
+				totalIcms27 = baseIcms27 * 0.27;
+				totalIcms26 = Double.parseDouble(strings[6].replaceAll(",", "."));
+				strings[6] = String.format("%.2f", totalIcms27);
+				totalIcms27 = totalIcms27 - totalIcms26;
+
+				icmsAntigo = Double.parseDouble(arquivoArray.get(c100index)[21].replaceAll(",", ".")) + totalIcms27;
+				arquivoArray.get(c100index)[21] = String.format("%.2f", icmsAntigo);
+
+			} else if (strings[0].equals("C470") && strings[8].length() > 1
+					&& strings[8].substring(0, 2).equals("19")) {
+				strings[8] = "20";
+
+			} else if (strings[0].equals("C470") && strings[8].length() > 1
+					&& strings[8].substring(0, 2).equals("26")) {
+				strings[8] = "27";
+
+			} else if (strings[0].equals("C490") && strings[3].length() > 1
+					&& strings[3].substring(0, 2).equals("19")) {
+				strings[3] = "20";
+				double baseicms = Double.parseDouble(strings[5].replaceAll(",", "."));
+				strings[6] = String.format("%.2f", baseicms * 0.2);
+
+			} else if (strings[0].equals("C490") && strings[3].length() > 1
+					&& strings[3].substring(0, 2).equals("26")) {
+				strings[3] = "27";
+				double baseicms = Double.parseDouble(strings[5].replaceAll(",", "."));
+				strings[6] = String.format("%.2f", baseicms * 0.27);
+
+			} else if (strings[0].equals("C420") && strings[4].equals("Contador de Aliquota - 19,00%")) {
+				strings[4] = "Contador de Aliquota - 20,00%";
+				strings[1] = "T2000";
+
+			} else if (strings[0].equals("C420") && strings[4].equals("Contador de Aliquota - 26,00%")) {
+				strings[4] = "Contador de Aliquota - 27,00%";
+				strings[1] = "T2700";
 			}
 
 			indice++;
